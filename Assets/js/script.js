249b92f8c3755ea0c52bac253017a30a583c2fd5
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-11-21 15:42:27 -0500

    5 day weather

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,64 +1,3 @@
-// var apiKey = '1b84b3d807e22917f303632143eabcc6'
-// var searchBtn = $('#searchBtn')
-// var searchInput = ''
-// var cityName = ''
-// var currentWeather = ''
-// var today = dayjs();
-// $('#currentDate').text(today.format('dddd, MMM D, YYYY'));
-
-
-// searchBtn.click(function(){
-//     console.log("btn was clicked")
-//     searchInput = $('#searchCity').val().trim();
-//     console.log(searchInput)
-//     getLocation(searchInput)
-// })
-
-
-// function getLocation(searchInput) {
-//     var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=1&appid=" + apiKey;
-//     console.log(searchInput)
-//     fetch(apiUrl).then(function(response) {
-//         if (response.ok) {
-//             response.json().then(function(data) {
-//                 console.log(data)
-//                 var latitude = data[0].lat;
-//                 var longitude = data[0].lon;
-//                 var latString = latitude.toString();
-//                 var lonString = longitude.toString();
-//                 var cityName = document.getElementById('cityName')
-//                 cityName.textContent = data[0].name;
-//                 getWeather(latString, lonString)
-//             })
-//         }
-//     })
-
-// }
-// function getWeather(lat, lon) {
-//     var apiKey = '1b84b3d807e22917f303632143eabcc6'
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
-//     fetch(apiUrl).then(function(response) {
-//         if (response.ok) {
-//             response.json().then(function(main) {
-//                 console.log(main)
-//                 var temperature = main.main.temp;
-//                 var windSpeed = main.wind.speed;
-//                 var humidity = main.main.humidity;
-//                 var tempString = temperature.toString();
-//                 var windString = windSpeed.toString();
-//                 var humidityString = humidity.toString();
-//                 var currentWeather = document.getElementById('currentWeather')
-//                 // currentWeather.textContent = main.name;
-//                 getWeather(tempString, windString, humidityString)
-
-//             })
-//         }
-//     })
-
-// }
-
-
-
 var apiKey = '1b84b3d807e22917f303632143eabcc6'
 var searchBtn = $('#searchBtn')
 var searchInput = ''
@@ -77,7 +16,7 @@ searchBtn.click(function(){
 
 
 function getLocation(searchInput) {
-    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=1&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
     console.log(searchInput)
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
@@ -97,25 +36,28 @@ function getLocation(searchInput) {
 }
 function getWeather(lat, lon) {
     var apiKey = '1b84b3d807e22917f303632143eabcc6'
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(main) {
                 console.log(main)
-                var temperature = main.main.temp;
-                var windSpeed = main.wind.speed;
-                var humidity = main.main.humidity;
+                for (var i = 0; i < 5; i++) {
+                var temperature = main.list[i * 8].main.temp;
+                console.log(temperature)
+                var windSpeed = main.list[i * 8].wind.speed;
+                var humidity = main.list[i * 8].main.humidity;
                 var tempString = temperature.toString();
                 var windString = windSpeed.toString();
                 var humidityString = humidity.toString();
-                var todaysTemp = document.getElementById('todaysTemp')
+                var todaysTemp = document.getElementById('day' + i + 'Temp')
+                console.log(todaysTemp);
                 todaysTemp.textContent = temperature;
-                var todaysWind = document.getElementById('todaysWind')
+                var todaysWind = document.getElementById('day' + i + 'Wind')
                 todaysWind.textContent = windSpeed;
-                var todaysHumidity = document.getElementById('todaysHumidity')
+                var todaysHumidity = document.getElementById('day' + i + 'Humidity')
                 todaysHumidity.textContent = humidity;
                 // getWeather(tempString, windString, humidityString)
-
+                }
             })
         }
     })
