be32c711abb8ef396e60830b55b85ec72a34f7ab
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-11-21 18:57:34 -0500

    JavaScript refactoring

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,9 @@ var apiKey = '1b84b3d807e22917f303632143eabcc6'
 var searchBtn = $('#searchBtn')
 var searchInput = ''
 var cityName = ''
-var currentWeather = ''
+// var currentWeather = ''
 var today = dayjs();
-$('#currentDate').text(today.format('dddd, MMM D, YYYY'));
+$('#currentDate').text(today.format('(DD/MM/YYYY)'));
 
 
 searchBtn.click(function(){
@@ -36,26 +36,47 @@ function getLocation(searchInput) {
 }
 function getWeather(lat, lon) {
     var apiKey = '1b84b3d807e22917f303632143eabcc6'
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + '&units=imperial';
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(main) {
                 console.log(main)
                 for (var i = 0; i < 5; i++) {
-                var temperature = main.list[i * 8].main.temp;
-                console.log(temperature)
-                var windSpeed = main.list[i * 8].wind.speed;
-                var humidity = main.list[i * 8].main.humidity;
-                var tempString = temperature.toString();
-                var windString = windSpeed.toString();
-                var humidityString = humidity.toString();
-                var todaysTemp = document.getElementById('day' + i + 'Temp')
-                console.log(todaysTemp);
-                todaysTemp.textContent = temperature;
-                var todaysWind = document.getElementById('day' + i + 'Wind')
-                todaysWind.textContent = windSpeed;
-                var todaysHumidity = document.getElementById('day' + i + 'Humidity')
-                todaysHumidity.textContent = humidity;
+                   var fiveDayContainer = document.getElementById('fiveDayForecastContainer')
+                   var card = document.createElement('div')
+                   var temp = document.createElement('p')
+                   var wind = document.createElement('p')
+                   var humidity = document.createElement('p')
+                   var tempSyle = `Temp: ${temp} ℉`;
+
+                   temp.innerHTML = main.list[i * 8].main.temp;
+                   wind.innerHTML = main.list[i * 8].wind.speed;
+                   humidity.innerHTML = main.list[i * 8].main.humidity;
+                   
+                   card.classList.add('card')
+                   card.appendChild(temp)
+                   card.appendChild(wind)
+                   card.appendChild(humidity)
+                   console.log(main.list[i * 8].wind.speed)
+                   console.log(main.list[i * 8].main.humidity)
+                   
+                   
+                   fiveDayContainer.appendChild(card)
+
+                // var temperature = main.list[i * 8].main.temp;
+                // console.log(temperature)
+                // var windSpeed = main.list[i * 8].wind.speed;
+                // var humidity = main.list[i * 8].main.humidity;
+                // var tempString = temperature.toString();
+                // var windString = windSpeed.toString();
+                // var humidityString = humidity.toString();
+                // var todaysTemp = document.getElementById('day' + i + 'Temp')
+                // console.log(todaysTemp);
+                // todaysTemp.textContent = temperature;
+                // var todaysWind = document.getElementById('day' + i + 'Wind')
+                // todaysWind.textContent = windSpeed;
+                // var todaysHumidity = document.getElementById('day' + i + 'Humidity')
+                // todaysHumidity.textContent = humidity;
                 // getWeather(tempString, windString, humidityString)
                 }
             })
