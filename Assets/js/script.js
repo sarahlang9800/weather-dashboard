305fa4745925f08922648d8edd38b3cef0f6d8c5
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-11-21 20:32:55 -0500

    HTML refactored

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,27 +1,29 @@
 var apiKey = '1b84b3d807e22917f303632143eabcc6'
-var searchBtn = $('#searchBtn')
 var searchInput = ''
 var cityName = ''
-// var currentWeather = ''
 var today = dayjs();
 $('#currentDate').text(today.format('(DD/MM/YYYY)'));
 
+var searchBtn = $('#searchBtn')
 
-searchBtn.click(function(){
-    console.log("btn was clicked")
+searchBtn.on('click', function(event){
+    event.preventDefault();
     searchInput = $('#searchCity').val().trim();
-    console.log(searchInput)
     getLocation(searchInput)
+    localStorage.setItem(searchInput, cityName)
 })
+
+function renderInput() {
+    $('#searchCity').val(localStorage.getItem(cityName))
+}
+
 
 
 function getLocation(searchInput) {
     var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
-    console.log(searchInput)
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data)
                 var latitude = data[0].lat;
                 var longitude = data[0].lon;
                 var latString = latitude.toString();
@@ -40,44 +42,30 @@ function getWeather(lat, lon) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(main) {
-                console.log(main)
                 for (var i = 0; i < 5; i++) {
                    var fiveDayContainer = document.getElementById('fiveDayForecastContainer')
                    var card = document.createElement('div')
+                   var date = document.createElement('p')
                    var temp = document.createElement('p')
                    var wind = document.createElement('p')
                    var humidity = document.createElement('p')
-                   var tempSyle = `Temp: ${temp} ℉`;
 
-                   temp.innerHTML = main.list[i * 8].main.temp;
-                   wind.innerHTML = main.list[i * 8].wind.speed;
-                   humidity.innerHTML = main.list[i * 8].main.humidity;
+                   var dateText = today.add(i, 'day').format('(DD/MM/YYYY)');  
+                   date.innerHTML = dateText;
+                     
+                   temp.innerHTML = `Temp: ${main.list[i * 8].main.temp} ℉`;
+                   wind.innerHTML = `Wind: ${main.list[i * 8].wind.speed} MPH`;
+                   humidity.innerHTML = `Humidity: ${main.list[i * 8].main.humidity} %`;
                    
                    card.classList.add('card')
+                   card.appendChild(date)
                    card.appendChild(temp)
                    card.appendChild(wind)
                    card.appendChild(humidity)
-                   console.log(main.list[i * 8].wind.speed)
-                   console.log(main.list[i * 8].main.humidity)
-                   
                    
                    fiveDayContainer.appendChild(card)
 
-                // var temperature = main.list[i * 8].main.temp;
-                // console.log(temperature)
-                // var windSpeed = main.list[i * 8].wind.speed;
-                // var humidity = main.list[i * 8].main.humidity;
-                // var tempString = temperature.toString();
-                // var windString = windSpeed.toString();
-                // var humidityString = humidity.toString();
-                // var todaysTemp = document.getElementById('day' + i + 'Temp')
-                // console.log(todaysTemp);
-                // todaysTemp.textContent = temperature;
-                // var todaysWind = document.getElementById('day' + i + 'Wind')
-                // todaysWind.textContent = windSpeed;
-                // var todaysHumidity = document.getElementById('day' + i + 'Humidity')
-                // todaysHumidity.textContent = humidity;
-                // getWeather(tempString, windString, humidityString)
+                   
                 }
             })
         }
